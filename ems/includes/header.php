4e7e2286f45f1997f288d30f29700bdc265b075c
Nitin Prakash <nitinprakash16@gmail.com>
2024-06-25 03:05:46 +0530

    Add files via upload

---
+++ ems/includes/header.php
@@ -0,0 +1,49 @@
+<div class="sidebar">
+      <div class="logo-details">
+      <img src="assets/img/emslogo.png" />
+      </div>
+      <ul class="nav-links">
+
+        <li>
+          <a href="dashboard.php" class="active">
+            <i class="bx bx-grid-alt"></i>
+            <span class="links_name">Dashboard</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="listed-equipment.php" class="active">
+            <i class="bx bx-grid-alt"></i>
+            <span class="links_name">Equipment List</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="issued-equipment.php" class="active">
+            <i class="bx bx-grid-alt"></i>
+            <span class="links_name">Issued Equipment</span>
+          </a>
+        </li>
+
+
+        <li>
+        <a href="#" id="ddlmenuItem" data-toggle="dropdown"> <i class="bx bx-grid-alt"></i><span class="links_name">Account</span><i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="my-profile.php">My Profile</a></li>
+                                    <li><a href="change-password.php">Change Password</a></li>
+                                </ul>
+        </li>
+
+        
+
+        <li class="log_out">
+          <a href="logout.php">
+            <i class="bx bx-grid-alt"></i>
+            <span class="links_name">Logout</span>
+          </a>
+        </li>
+
+
+      </ul>
+
+    </div>
---
+++ ems/includes/sidebar-home.php
@@ -0,0 +1,40 @@
+<div class="sidebar">
+      <div class="logo-details">
+      <img src="assets/img/emslogo.png" />
+      </div>
+      <ul class="nav-links">
+
+        <li>
+          <a href="dashboard.php" class="active">
+            <i class="bx bx-grid-alt"></i>
+            <span class="links_name">Home</span>
+          </a>
+        </li>
+
+
+        <li>
+        <a href="#" id="ddlmenuItem" data-toggle="dropdown"> <i class="bx bx-grid-alt"></i><span class="links_name">User</span><i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="userlogin.php">User Login</a></li>
+                                    <li><a href="signup.php">User Signup</a></li>
+                                </ul>
+        </li>
+
+        <li>
+          <a href="adminlogin.php" class="active">
+            <i class="bx bx-grid-alt"></i>
+            <span class="links_name">Admin login</span>
+          </a>
+        </li>
+
+        <li class="log_out">
+          <a href="devopt.php">
+            <i class="bx bx-grid-alt"></i>
+            <span class="links_name">Dev. Opt</span>
+          </a>
+        </li>
+
+
+      </ul>
+
+    </div>
