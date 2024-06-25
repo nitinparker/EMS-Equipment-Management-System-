83d0ec219adb998ede915fa6bcbe6501dcc0729e
Nitin Prakash <nitinprakash16@gmail.com>
2024-06-25 16:30:54 +0530

    Add files via upload

---
+++ ems/admin/assets/js/custom.js
@@ -0,0 +1,66 @@
+﻿
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% Free To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".sidebarBtn");
+    sidebarBtn.onclick = function() {
+      sidebar.classList.toggle("active");
+      if(sidebar.classList.contains("active")){
+      sidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
+    }else
+      sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
+
+
+
+
+
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        slide_fun: function () {
+
+            $('#carousel-example').carousel({
+                interval:3000 // THIS TIME IS IN MILLI SECONDS
+            })
+
+        },
+        dataTable_fun: function () {
+
+            $('#dataTables-example').dataTable();
+
+        },
+       
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+            
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.slide_fun();
+        mainApp.dataTable_fun();
+        mainApp.custom_fun();
+    });
+}(jQuery));
+
+
